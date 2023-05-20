cac4887ddc3355b450c6bdf325c9027af8f227eb
Luiz Felipe Moureau <xfazxtudo@gmail.com>
2023-05-20 17:30:42 -0300

    structure: demo home page

--- web/src/Application.tsx
+++ web/src/Application.tsx
@@ -1,6 +1,9 @@
 /* ---------- External ---------- */
 import Nullstack, { NullstackClientContext, NullstackNode } from 'nullstack';
 
+/* ---------- Modules ---------- */
+import { Home } from '_modules/home';
+
 /* ---------- Styles ---------- */
 import '/public/fonts/styles.css';
 import '_styles/global.css';
@@ -8,22 +11,21 @@ import '_styles/global.css';
 declare function Head(): NullstackNode;
 
 class Application extends Nullstack {
-  initiate({ page }: NullstackClientContext) {
-    page.locale = 'en-US';
-  }
-
-  prepare({ page }: NullstackClientContext) {
-    page.title = 'Moureau Dev';
-    page.description = "Luiz Felipe's Moureau personal website";
-  }
-
+  /* ---------- Renderers ---------- */
   renderHead({ page }: NullstackClientContext) {
     return (
       <head>
-        <meta name="description" content={page.description} />
+        <meta
+          name="keywords"
+          content="Luiz Felipe Moureau, web, developer, portfolio, blog, resume, contact, contact me, moureau, mococa, luizfelipesmoureau"
+        />
 
+        <meta name="theme-color" content="#1b1f24" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@luizmoureau" />
+        <meta name="twitter:title" content={page.title} />
+        <meta name="twitter:description" content={page.description} />
         <link href="https://fonts.gstatic.com" rel="preconnect" />
-
         <link
           href="https://fonts.googleapis.com/css2?family=Crete+Round&family=Roboto&display=swap"
           rel="stylesheet"
@@ -32,15 +34,27 @@ class Application extends Nullstack {
     );
   }
 
+  /* ---------- Life cycle ---------- */
+  async initiate({ page }: NullstackClientContext) {
+    page.locale = 'en-US';
+    page.title = 'Moureau - Fullstack Developer';
+    page.description =
+      "I'm Luiz Felipe Moureau and this is my personal website. Here you can read about me, my passions, work and more.\nFeel free to contact me anytime.";
+  }
+
+  /* ---------- Render ---------- */
   render() {
     return (
-      <>
+      <body>
         <Head />
 
-        <main>
-          <h1>Hello World</h1>
-        </main>
-      </>
+        <hello>
+          Greetings, inspector! If you wanna see more, check my Github: mococa
+          😏
+        </hello>
+
+        <Home route="/" />
+      </body>
     );
   }
 }
